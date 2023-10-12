82f1665965de648de07784a125a3f10c2e8486e8
J Kimball <jeffkimballis@gmail.com>
2023-10-12 10:58:39 -0500

    add expense filter component to select dates

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate' 
 import Card from '../UI/Card'
-import {useState} from 'react'
 
 const ExpenseItem = ({title, amount, date}) => {
-    const [titleOutput, setTitleOutput] = useState(title)
-
-    const changeTitleHandler = () =>{
-        setTitleOutput("Title Changed") 
-    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={date}/>
             <div className='expense-item__description'>
-                <h2>{titleOutput}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
-            </div>  
-            <button onClick={changeTitleHandler}>ChangeTitle</button>          
+            </div>         
         </Card>
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,48 @@
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
+import ExpenseFilter from './ExpenseFilter'
+import { useState } from 'react'
 
 const Expenses = ({ items }) => {
+    const [filteredYear, setFilteredYear] = useState('2022')
+
+    const getFilteredYear = (selectedYear) => {
+        setFilteredYear(selectedYear)
+    }
+
     return (
-        <Card className='expenses'>
-            <ExpenseItem 
-                key={items[0].id} 
-                title={items[0].title} 
-                amount={items[0].amount} 
-                date={items[0].date} 
-            />
-            <ExpenseItem 
-                key={items[1].id} 
-                title={items[1].title} 
-                amount={items[1].amount} 
-                date={items[1].date} 
-            />
-            <ExpenseItem 
-                key={items[2].id} 
-                title={items[2].title} 
-                amount={items[2].amount} 
-                date={items[2].date} 
-            />
-            <ExpenseItem 
-                key={items[3].id} 
-                title={items[3].title} 
-                amount={items[3].amount} 
-                date={items[3].date} 
-            />
-        </Card>
+        
+            <div>                            
+                <Card className='expenses'>
+                <ExpenseFilter selected={filteredYear} onYearSelect={getFilteredYear}/>
+                    <ExpenseItem 
+                        key={items[0].id} 
+                        title={items[0].title} 
+                        amount={items[0].amount} 
+                        date={items[0].date} 
+                    />
+                    <ExpenseItem 
+                        key={items[1].id} 
+                        title={items[1].title} 
+                        amount={items[1].amount} 
+                        date={items[1].date} 
+                    />
+                    <ExpenseItem 
+                        key={items[2].id} 
+                        title={items[2].title} 
+                        amount={items[2].amount} 
+                        date={items[2].date} 
+                    />
+                    <ExpenseItem 
+                        key={items[3].id} 
+                        title={items[3].title} 
+                        amount={items[3].amount} 
+                        date={items[3].date} 
+                    />
+                </Card>
+        </div>
+        
     )
 }
 
